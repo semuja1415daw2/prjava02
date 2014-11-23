1ccb7d0765b8a78259939e9b527da75b45d16163
semuja1415daw2 <semuja1415daw2@gmail.com>
2014-11-23 20:45:55 +0100

    Treballant amb ordres Git i la branca01 v0.1

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -17,9 +17,6 @@ public class Prjava02 {
 */
 public static void main(String[] args) {
 System.out.println("versió 0.1 del projecte prjava02"); 
-System.out.println("Creació d'una branca del projecte prjava02");
-System.out.println("Afegint més codi a la branca00 del projecte prjava02");
-System.out.println("Afegint codi a la branca01 del projecte prjava02");
 try {
 InetAddress adreça = InetAddress.getLocalHost();
 String ipAddr = adreça.getHostAddress();
@@ -30,6 +27,9 @@ System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
 System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
 System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
 System.out.println("Versió OS: " + System.getProperty("os.version"));
+System.out.println("Creació d'una branca del projecte prjava02");
+System.out.println("Afegint més codi a la branca00 del projecte prjava02");
+System.out.println("Afegint codi a la branca01 del projecte prjava02");
 }
 catch (IOException e) {
 }  
