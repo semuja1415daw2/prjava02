e6abee4318d47c08730c39b3bb16ea1a39f3a8b2
semuja1415daw2 <semuja1415daw2@gmail.com>
2014-11-23 20:33:13 +0100

    Branca01

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -19,6 +19,7 @@ public static void main(String[] args) {
 System.out.println("versió 0.1 del projecte prjava02"); 
 System.out.println("Creació d'una branca del projecte prjava02");
 System.out.println("Afegint més codi a la branca00 del projecte prjava02");
+System.out.println("Afegint codi a la branca01 del projecte prjava02");
 try {
 InetAddress adreça = InetAddress.getLocalHost();
 String ipAddr = adreça.getHostAddress();
@@ -27,7 +28,6 @@ System.out.println("hostname="+hostname);
 System.out.println("Adreça IP: " + ipAddr);
 System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
 System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
-System.out.println("Conflicte típic: Codi diferent a una mateixa línia");
 System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
 System.out.println("Versió OS: " + System.getProperty("os.version"));
 }
